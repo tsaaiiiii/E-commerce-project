6aed0ad98f400f928c0046dc76b614061b5e1d8f
“ting” <a0909513991@gmail.com>
2023-02-01 01:02:26 +0800

    完成2-7頁，第一次測試

---
+++ page7.js
@@ -0,0 +1,8 @@
+$(".ec").click(function(){
+    $("#address").css("display","none");
+    $("#mail_in").show();
+    });
+$(".deliver").click(function(){
+    $("#address").css("display","block");
+    $("#mail_in").hide();
+    });
